0f310511e405022369bfb8f161b69d753d7c8f5c
shayne243 <marques0@purdue.edu>
2020-06-28 12:04:57 +0400

    new book can be added to library

--- script.js
+++ script.js
@@ -4,32 +4,30 @@ let bookSection = document.getElementById("book-section");
 let newBook = document.getElementById("new-book");
 let bookForm = document.getElementById("new-book-form");
 let submit = document.getElementById("submit");
+let form = document.getElementById("book-form");
 
-function Book(name, author, year, genre) {
+function Book(name, author, year, pages) {
     this.name = name;
     this.author = author;
     this.year = year;
-    this.genre = genre;
+    this.pages = pages;
 }
 
 function submitBtn() {
-    let name = "";
-    let author = "";
-    let year = 0;
-    let genre = "";
-    name = document.getElementById("title").value;
-    author = document.getElementById("author").value;
-    genre = document.getElementById("genre").value;
-    year = document.getElementById("year").value;
+    let inputList = form.getElementsByTagName("input");
+    addBook(inputList[0].value, inputList[1].value, inputList[2].value, inputList[3].value);
+    render();
+    bookForm.setAttribute("style", "display: none;")
+    form.reset();
 }
 
 function setSubmitBtn() {
     submit.addEventListener("click", submitBtn);
 }
 
-function addBook(name, author, year, genre) {
-    let book = new Book(name, author, year, genre);
-    library.push(book);
+function addBook(name, author, year, pages) {
+    let book = new Book(name, author, year, pages);
+    library.unshift(book);
 }
 
 function displayForm() {
@@ -41,33 +39,39 @@ function setNewBookBtn() {
 }
 
 function render() {
-    for (let i = 0; i < library.length; ++i) {
-        let bookDiv = document.createElement('div');
-        let bookTitle = document.createElement('h1');
-        bookTitle.innerHTML = library[i].name;
-        let bookAuthor = document.createElement('h2');
-        bookAuthor.innerHTML = library[i].author;
-        let bookInfo = document.createElement('h3');
-        bookInfo.innerHTML = library[i].genre + ", " + library[i].year;
-        bookDiv.appendChild(bookTitle);
-        bookDiv.appendChild(bookAuthor);
-        bookDiv.appendChild(bookInfo);
-        bookDiv.setAttribute("style", "box-shadow: 0px 0px 47px -14px rgba(0,0,0,0.75); border: solid 1px white; border-radius: 10px; margin: auto; width: 200px;")
-        bookSection.setAttribute("style", "margin: auto; display: grid; grid-template-columns: repeat(5, 250px); gap: 50px; justify-content: space-evenly")
-        bookSection.appendChild(bookDiv);
-    } 
+    let bookDiv = document.createElement('div');
+    let bookTitle = document.createElement('h2');
+    bookTitle.innerHTML = library[0].name;
+    let bookAuthor = document.createElement('h2');
+    bookAuthor.innerHTML = library[0].author;
+    let bookInfo = document.createElement('p');
+    bookInfo.innerHTML = "PAGES: " + library[0].pages + "<br>YEAR: " + library[0].year;
+    bookDiv.appendChild(bookTitle);
+    bookDiv.appendChild(bookAuthor);
+    bookDiv.appendChild(bookInfo);
+    bookDiv.setAttribute("style", "box-shadow: 0px 0px 47px -14px rgba(0,0,0,0.75); border: solid 1px white; border-radius: 10px; margin: auto; width: 200px;")
+    bookSection.setAttribute("style", "margin: auto; display: grid; grid-template-columns: repeat(5, 250px); gap: 50px; justify-content: space-evenly");        bookSection.appendChild(bookDiv);
 }
 
-addBook("hi", "hi", "hi", "hi");
-addBook("hi", "hi", "hi", "hi");
-addBook("hi", "hi", "hi", "hi");
-addBook("hi", "hi", "hi", "hi");
-addBook("hi", "hi", "hi", "hi");
-addBook("hi", "hi", "hi", "hi");
-addBook("hi", "hi", "hi", "hi");
-addBook("hi", "hi", "hi", "hi");
-addBook("hi", "hi", "hi", "hi");
-addBook("hi", "hi", "hi", "hi");
-render();
+addBook("hi", "hi", 2000, 2000);
+addBook("hi", "hi", 2000, 2000);
+addBook("hi", "hi", 2000, 2000);
+addBook("hi", "hi", 2000, 2000);
+addBook("hi", "hi", 2000, 2000);
+for (let i = 0; i < library.length; ++i) {
+    let bookDiv = document.createElement('div');
+    let bookTitle = document.createElement('h2');
+    bookTitle.innerHTML = library[i].name;
+    let bookAuthor = document.createElement('h2');
+    bookAuthor.innerHTML = library[i].author;
+    let bookInfo = document.createElement('p');
+    bookInfo.innerHTML = "PAGES: " + library[i].pages + "<br>YEAR: " + library[i].year;
+    bookDiv.appendChild(bookTitle);
+    bookDiv.appendChild(bookAuthor);
+    bookDiv.appendChild(bookInfo);
+    bookDiv.setAttribute("style", "box-shadow: 0px 0px 47px -14px rgba(0,0,0,0.75); border: solid 1px white; border-radius: 10px; margin: auto; width: 200px;")
+    bookSection.setAttribute("style", "margin: auto; display: grid; grid-template-columns: repeat(5, 250px); gap: 50px; justify-content: space-evenly")
+    bookSection.appendChild(bookDiv);
+}
 setNewBookBtn();
 setSubmitBtn();
